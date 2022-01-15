19eedd2745d47fe01863ede402d7743ef4b950c0
Miguel Angel Camacho <miguel-angelcr@hotmail.com>
2022-01-14 21:26:06 -0500

    database

--- src/database.js
+++ src/database.js
@@ -1,20 +1,25 @@
 import { connect } from "mongoose";
 // import { MONGODB_URI } from "./config";
-
-
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://angel:<macr1995>@cluster0.y7bmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 // const MONGODB_URI = "mongodb+srv://angel:<macr1995>@cluster0.y7bmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const MONGODB_URI = "mongodb+srv://miguel:<macr1995>@cluster0.gqzcy.mongodb.net/segundomongoapp?retryWrites=true&w=majority"
+
+
+// const mongoose = require('mongoose');
+
+// const usuario = "usuariodb"
+// const password = "passdb"
+// const dbName = "veterinaria"
+
+// const uri = `mongodb+srv://${usuario}:${password}@cluster0.ncdk5.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(()=> console.log('conectado a mongodb')) 
+//   .catch(e => console.log('error de conexión', e))
+
 
 (async () => {
   try {
-    const db = await connect(uri);
+    const db = await connect(MONGODB_URI);
     console.log("DB connected to", db.connection.name);
   } catch (error) {
     console.log(error);
