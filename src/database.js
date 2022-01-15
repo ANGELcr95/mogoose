ab6ea11f3f363338a796d2bc737d2cd4e29bc307
Miguel Angel Camacho <miguel-angelcr@hotmail.com>
2022-01-14 20:32:43 -0500

    connect

--- src/database.js
+++ src/database.js
@@ -1,10 +1,20 @@
 import { connect } from "mongoose";
 // import { MONGODB_URI } from "./config";
-const MONGODB_URI = "mongodb+srv://angel:<macr1995>@cluster0.y7bmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://angel:<macr1995>@cluster0.y7bmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+// const MONGODB_URI = "mongodb+srv://angel:<macr1995>@cluster0.y7bmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 (async () => {
   try {
-    const db = await connect(MONGODB_URI);
+    const db = await connect(uri);
     console.log("DB connected to", db.connection.name);
   } catch (error) {
     console.log(error);
